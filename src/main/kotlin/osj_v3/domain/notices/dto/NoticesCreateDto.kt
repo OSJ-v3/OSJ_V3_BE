cfeb6763b6e29e754be4eeef779781b97650a719
ajswldp <kkhhjj1209@gmail.com>
2025-12-19 20:23:56 +0900

    오타수정

--- src/main/kotlin/osj_v3/domain/notices/dto/NoticesCreateDto.kt
+++ src/main/kotlin/osj_v3/domain/notices/dto/NoticesCreateDto.kt
@@ -6,6 +6,6 @@ data class NoticesCreateDto(
     @field:NotBlank(message = "제목은 비어있을 수 없습니다.")
     val title: String,
 
-    @field:NotBlank(message = "제목은 비어있을 수 없습니다.")
+    @field:NotBlank(message = "내용은 비어있을 수 없습니다.")
     val content: String
 )
